6f5157769a60b840297baba8c1772408f5e9d1fd
gzmknk <gzmknk0@gmail.com>
2021-12-20 02:23:39 +0300

    Add project files.

---
+++ Blog.Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Blog.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Blog/Configuration/AppServices.cs
@@ -0,0 +1,23 @@
+﻿
+using Blog.Data;
+using Blog.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blog.Configuration
+{
+    public static class AppServices {
+        public static void AddDefaultServices(this IServiceCollection servicesCollection , IConfiguration configuration )
+        {
+            servicesCollection.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+            servicesCollection.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            servicesCollection.AddControllersWithViews().AddRazorRuntimeCompilation();
+            servicesCollection.AddRazorPages();
+        }
+    }
+}
