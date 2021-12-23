270c40aa020c20fb36f1c295a462b82931260935
gzmknk <gzmknk0@gmail.com>
2021-12-24 01:33:15 +0300

    Admin Paneli

---
+++ Blog/ViewComponents/AdminSideNavViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blog.ViewComponents
+{
+    public class AdminSideNavViewComponent : ViewComponent {
+        public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.Factory.StartNew(() => { return View(); });
+      
+    }
+
+
+    }
+}
